3b3af41e54639a14a3a6d0f5a2501337dfff8d8f
Ryan <ryoliver7@gmail.com>
2018-12-03 23:18:29 -0500

    ...

--- src/FindAllNumbers.java
+++ src/FindAllNumbers.java
@@ -1,14 +1,20 @@
 import java.util.ArrayList;
 import java.util.List;
 
+/* Given an array of integers where 1 ≤ a[i] ≤ n (n = size of array),
+some elements appear twice and others appear once.
+Find all the elements of [1, n] inclusive that do not appear in this array. */
+
 public class FindAllNumbers {
 
+    // Test method
     public static void main(String[] args) {
         int[] list = {4,3,2,7,8,2,3,1};
         System.out.print(findDisappearedNumbers(list).toString());
     }
+
     public static List<Integer> findDisappearedNumbers(int[] nums) {
-        List<Integer> answer = new ArrayList<Integer>();
+        List<Integer> answer = new ArrayList<>();
         int[] list = new int[nums.length + 1];
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] <= nums.length && nums[i] > 0)
